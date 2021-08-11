433db63f30295a355a56e12afb5a26d18ee9abe1
Colleensdan <43320990+Colleensdan@users.noreply.github.com>
2021-08-11 18:48:43 +0200

    saved validation files in InputForML dir

--- ShanleySummerStudent21/RScripts/improve_group_imbalances.py
+++ ShanleySummerStudent21/RScripts/improve_group_imbalances.py
@@ -5,21 +5,22 @@ There are clear differences between the number of HD, and WT mice. This can be a
 
 from imblearn.over_sampling import SVMSMOTE
 from pandas import read_csv
-from varname import nameof
 from glob import glob
 from ML import *
+from shutil import copy2
 
 
 
-def perform_smote(RNA, X_file_name, y_file_name):
+
+def perform_smote(RNA, X_file_name, y_file_name, d):
     X, y = get_X_y(RNA)
     X= X.drop(columns ="Samples")
     X_resampled, y_resampled = SVMSMOTE().fit_resample(X, y)
     X_resampled = X_resampled.drop_duplicates()
     y_resampled = y_resampled.iloc[X_resampled.index]
 
-# save files
-    loc = "../../../../InputForML/SMOTE/{}.csv"
+    # save files
+    loc = "../../../../InputForML/"+d+"/{}.csv"
 
     X_resampled.to_csv(loc.format(X_file_name))
     y_resampled.to_csv(loc.format(y_file_name))
@@ -37,17 +38,21 @@ def oversample(df, n):
     df =df.append(df.loc[added.index])
     return df.drop_duplicates
 
-dir = r"../Early Detection/Data/FilteredData/age"
+dir = r"../Early Detection/Data/FilteredData/outliers"
 chdir(dir)
-for filename in glob.glob('*train*'):
-    with open(os.path.join(os.getcwd(), filename), 'r') as f:
-        rna = read_csv(f)
-        n = filename.replace(".csv", "")
 
-        # duplicate data so enough values present for SMOTE, duplicate rather than random oversample to avoid
-        # potential biases from oversampling
-        rna = rna.append(rna)
+for d in ["outliers/", "no_outliers/"]:
+    chdir("../"+d)
+    for filename in glob.glob('*train*'):
+        with open(os.path.join(os.getcwd(), filename), 'r') as f:
+            rna = read_csv(f)
+            n = filename.replace(".csv", "")
 
-        # then duplicates are deleted here
-        perform_smote(rna, ("X_"+n), ("y_"+n))
+            # duplicate data so enough values present for SMOTE, duplicate rather than random oversample to avoid
+            # potential biases from oversampling
+            rna = rna.append(rna)
 
+            # then duplicates are deleted here
+            perform_smote(rna, ("X_"+n), ("y_"+n), d)
+    for filename in glob.glob('*validation*'):
+        copy2(filename, r"../../../../InputForML/"+d)
